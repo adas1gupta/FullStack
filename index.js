eaa70b8877baa32747c41a5e1babb660d635e057
Arjun Dasgupta <dasgupta1arjun@gmail.com>
2024-05-20 00:29:52 -0400

    Attemping to make index.js and package.json to be the root of the repository

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/',(req,res) => { 
+//get is retrieving a new route handler
+// '/' is the name of the host; you can have something like '/category'
+// req is request from someone; res is response to someone
+// res.send... closes request and sends back response of hi: there 
+    res.send({hi: 'there'});
+});
+
+const PORT = process.env.PORT || 5000; //by default, use value of 5000
+app.listen(PORT); //incoming traffic on port 5000 (localhost:5000 in google)
+
