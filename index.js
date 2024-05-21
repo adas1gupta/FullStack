3d6cd8a4c41cb058639b6bee0c2fe4cf1e86e5f6
Arjun Dasgupta <dasgupta1arjun@gmail.com>
2024-05-21 18:27:12 -0400

    updated server submodule and index.js files after the incorporation of the routes and services directories

--- index.js
+++ index.js
@@ -1,38 +1,18 @@
 const express = require('express');
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const keys = require('./server/config/keys');
+require('./services/passport');
 
 const app = express();
 
-passport.use(new GoogleStrategy({
-    clientID: keys.googleClientID,
-    clientSecret: keys.googleClientSecret,
-    callbackURL: '/auth/google/callback'
-    }, 
-    (accessToken, refreshToken, profile, done) => {
-        console.log('access token',accessToken);
-        console.log('refresh token',refreshToken);
-        console.log('profile:', profile);
-    })
-);
+require('./routes/authRoutes')(app);
 
-app.get(
-    '/auth/google', 
-    passport.authenticate('google', {
-        scope: ['profile', 'email']
-    })
-);
-app.get('/auth/google/callback',
-    passport.authenticate('google')
-);
-app.get('/',(req,res) => { 
+
+//app.get('/',(req,res) => { 
 //get is retrieving a new route handler
 // '/' is the name of the host; you can have something like '/category'
 // req is request from someone; res is response to someone
 // res.send... closes request and sends back response of hi: there 
-    res.send({hi: 'there'});
-});
+//    res.send({hi: 'there'});
+//});
 
 const PORT = process.env.PORT || 5000; //by default, use value of 5000
 app.listen(PORT); //incoming traffic on port 5000 (localhost:5000 in google)
