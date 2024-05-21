c0ad959637ebd596df613388cbad70522917cadd
Arjun Dasgupta <dasgupta1arjun@gmail.com>
2024-05-21 12:00:28 -0400

    index.js file updated with auth/google portion

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 const express = require('express');
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./server/config/keys');
 
 const app = express();
 
-passport.use(new GoogleStrategy());
+passport.use(new GoogleStrategy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: '/auth/google/callback'
+    }, 
+    accessToken => {
+        console.log(accessToken);
+    })
+);
 
+app.get(
+    '/auth/google', 
+    passport.authenticate('google', {
+        scope: ['profile', 'email']
+    })
+);
 app.get('/',(req,res) => { 
 //get is retrieving a new route handler
 // '/' is the name of the host; you can have something like '/category'
