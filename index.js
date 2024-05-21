b707bdc31c70aa23951add92e71f772ad37da7e3
Arjun Dasgupta <dasgupta1arjun@gmail.com>
2024-05-21 16:54:21 -0400

    route handler to exchange code with Google's servers

--- index.js
+++ index.js
@@ -21,6 +21,9 @@ app.get(
         scope: ['profile', 'email']
     })
 );
+app.get('/auth/google/callback',
+    passport.authenticate('google')
+);
 app.get('/',(req,res) => { 
 //get is retrieving a new route handler
 // '/' is the name of the host; you can have something like '/category'
