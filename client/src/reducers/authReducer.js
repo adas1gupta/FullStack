d205bd0ba71e0aae70146130cecb3d3869b523ce
Arjun Dasgupta <dasgupta1arjun@gmail.com>
2024-05-24 22:47:02 -0400

    reducers

---
+++ client/src/reducers/authReducer.js
@@ -0,0 +1,9 @@
+function authReducer(state = {}, action) {
+    console.log(action);
+    switch (action.type) {
+        default:
+            return state;
+    }
+}
+
+export default authReducer;
